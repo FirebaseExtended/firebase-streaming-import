cfd03710268f5873071309927de7a00184d0ca64
Matthew Tse <mtse@google.com>
2014-12-17 13:42:25 -0800

    added chunk size argument

--- import.py
+++ import.py
@@ -7,30 +7,51 @@ import sys
 
 def main(args):
 
+    CHUNK_THRESHHOLD = args.chunk_size * 1024 * 1024
+
     parser = ijson.parse(open(args.json_file))
 
+    # for beginning/ending braces
+    currentObjSize = 2
     rootObj = {}
+
     for prefix, event, value in parser:
         if value is not None and event != 'map_key':
+
+            keys = prefix.split('.')
+
+            currObj = rootObj
+            for key in keys[:-1]:
+                if key in currObj:
+                    currObj = currObj[key]
+                    # Don't count the key size twice, but add a comma
+                    currentObjSize += 1
+                else:
+                    newObj = {}
+                    currObj[key] = newObj
+                    currObj = newObj
+                    # For size of key, quotes around key, colon, braces
+                    currentObjSize += len(key) + 2 + 1 + 2
 
             if event == 'number':
                 leafValue = float(value)
             else:
                 leafValue = value
+                # For quotes around string based leaf
+                currentObjSize += 2
+            currObj[keys[-1]] = leafValue
+            # For size of final key, quotes, colon
+            currentObjSize += len(keys[-1]) + 2 + 1
+            # For size of leaf value
+            currentObjSize += len(str(value))
 
-            prefixes = prefix.split('.')
+            if currentObjSize > CHUNK_THRESHHOLD:
+                patchToFirebase(rootObj, args)
+                rootObj = {}
+                currentObjSize = 2
 
-            currObj = rootObj
-            for prefix in prefixes[:-1]:
-                if prefix in currObj:
-                    currObj = currObj[prefix]
-                else:
-                    newObj = {}
-                    currObj[prefix] = newObj
-                    currObj = newObj
-            currObj[prefixes[-1]] = leafValue
-
-    patchToFirebase(rootObj, args)
+    if bool(rootObj):
+        patchToFirebase(rootObj, args)
 
 
 def patchToFirebase(dataObj, args):
@@ -45,9 +66,10 @@ def patchToFirebase(dataObj, args):
 
 
 if __name__ == '__main__':
-    argParser = argparse.ArgumentParser(description="Import a large json file into a Firebase via json Streaming.")
+    argParser = argparse.ArgumentParser(description="Import a large json file into a Firebase via json Streaming.  Uses HTTP PATCH requests.")
     argParser.add_argument('firebase_url', help="Specify the Firebase URL (e.g. https://test.firebaseio.com/dest/path).")
     argParser.add_argument('json_file', help="The JSON file to import.")
     argParser.add_argument('-a', '--auth', help="Optional Auth token if necessary to write to Firebase.")
+    argParser.add_argument('-c', '--chunk_size', default=10, type=int, help="Chunk size in Megabytes, defaults to 10MB.")
     args = argParser.parse_args()
     main(args)
