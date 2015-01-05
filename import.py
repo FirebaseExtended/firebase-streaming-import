b5a514e5c84282e7544315d5a8a0934653cecd50
Matthew Tse <mtse@google.com>
2015-01-05 13:27:56 -0800

    reverted back to writing only leaf values

--- import.py
+++ import.py
@@ -4,18 +4,13 @@ import argparse
 import json
 import sys
 import re
+import traceback
 
 
 def main(args):
 
-    CHUNK_THRESHHOLD = args.chunk_size * 1024 * 1024
-
     parser = ijson.parse(open(args.json_file))
     session = requests.Session()
-
-    # for beginning/ending braces
-    currentObjSize = 2
-    rootObj = {}
 
     for prefix, event, value in parser:
         if value is not None and event != 'map_key':
@@ -32,56 +27,34 @@ def main(args):
                 prefixList[index] = '.'
             prefix = "".join(prefixList)
             # 4. Split on whitespace
-            keys = prefix.split(' ')
+            prefixes = prefix.split(' ')
+            lastPrefix = prefixes[-1]
+            prefixes = prefixes[:-1]
 
-            currObj = rootObj
-            for key in keys[:-1]:
-                if key in currObj:
-                    currObj = currObj[key]
-                    # Don't count the key size twice, but add a comma
-                    currentObjSize += 1
-                else:
-                    newObj = {}
-                    currObj[key] = newObj
-                    currObj = newObj
-                    # For size of key, quotes around key, colon, braces
-                    currentObjSize += len(key) + 2 + 1 + 2
+            url = args.firebase_url
+            for prefix in prefixes:
+                url += prefix + '/'
+            url += '.json'
+            if args.silent:
+                url += '?print=silent'
 
             if event == 'number':
-                leafValue = float(value)
+                dataObj = {lastPrefix: float(value)}
             else:
-                leafValue = value
-                # For quotes around string based leaf
-                currentObjSize += 2
-            currObj[keys[-1]] = leafValue
-            # For size of final key, quotes, colon
-            currentObjSize += len(keys[-1]) + 2 + 1
-            # For size of leaf value
-            currentObjSize += len(str(value))
+                dataObj = {lastPrefix: value}
 
-            if currentObjSize > CHUNK_THRESHHOLD:
-                patchToFirebase(rootObj, args, session)
-                rootObj = {}
-                currentObjSize = 2
+            try:
+                if args.auth is not None:
+                    authObj = {'auth': args.auth}
+                    session.patch(url, data=json.dumps(dataObj), params=authObj)
+                else:
+                    session.patch(url, data=json.dumps(dataObj))
+            except Exception, e:
+                print('Caught an error: ' + traceback.format_exc())
+                print prefix, event, value
 
-    if bool(rootObj):
-        patchToFirebase(rootObj, args, session)
-
-
-def patchToFirebase(dataObj, args, session):
-    restURL = args.firebase_url + '/.json'
-    if args.silent:
-        restURL += '?print=silent'
-    if args.auth is not None:
-        authObj = {'auth': args.auth}
-        r = session.patch(restURL, data=json.dumps(dataObj), params=authObj)
-    else:
-        r = session.patch(restURL, data=json.dumps(dataObj))
-    if not args.silent:
-        print r.text
-    else:
-        sys.stdout.write('.')
-        sys.stdout.flush()
+            sys.stdout.write('.')
+            sys.stdout.flush()
 
 
 if __name__ == '__main__':
@@ -90,6 +63,5 @@ if __name__ == '__main__':
     argParser.add_argument('json_file', help="The JSON file to import.")
     argParser.add_argument('-a', '--auth', help="Optional Auth token if necessary to write to Firebase.")
     argParser.add_argument('-s', '--silent', action='store_true', help="Silences the server response, speeding up the connection.")
-    argParser.add_argument('-c', '--chunk_size', default=1, type=int, help="Chunk size in Megabytes, defaults to 1MB.")
     args = argParser.parse_args()
     main(args)
